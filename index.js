74f150dbaf23cbf8504f34f1da852e31a703e820
K5 <utkuzun50@gmail.com>
2022-09-04 14:02:18 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const evenOrOdd = (number) => {
+  let sum = 0
+
+  while (number > 0) {
+    mod = number % 10
+    sum = sum + mod
+    number = (number - mod) / 10
+  }
+
+  if (sum % 2 === 0) {
+    return 'even'
+  }
+
+  return 'odd'
+}
+
+const randomNumber = () => {
+  const upperLimit = 9999999
+  const number = Math.floor(Math.random() * upperLimit)
+  return number
+}
+
+if (process.argv.length > 2) {
+  const userNumber = process.argv[2]
+  const result = evenOrOdd(userNumber)
+  console.log(`${userNumber} is ${result}`)
+  return
+}
+
+const random = randomNumber()
+const result = evenOrOdd(random)
+console.log(`${random} is ${result}`)
